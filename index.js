5a10a14c0ad23e8b849325f368bd0efe6698a50e
sonusv7719 <sonu.s.v.771984@gmail.com>
2023-04-23 15:47:48 +0530

    attachment update

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const cors = require("cors");
 const app = express()
 const nodemailer = require("nodemailer");
+const multer = require('multer');
+const fs = require('fs');
 
+const upload = multer();
 const corsOptions = {
     origin: '*',
     credentials: true,            //access-control-allow-credentials:true
@@ -18,37 +21,93 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/sendemail', async(req, res) => {
-    const emailFrom = req.body.from;
-    const pass = req.body.pass;
-    const emailTo = req.body.to;
-    const content = req.body.content;
-    const htmlContent = req.body.html || "";
-    const subjectContent = req.body.subject;
+app.post('/sendemail', upload.single('file'), async (req, res) => {
+    try {
+        // console.log(req.file.buffer);
+        const emailFrom = req.body.from;
+        const pass = req.body.pass;
+        const emailTo = req.body.to;
+        const content = req.body.content;
+        const htmlContent = req.body.html || "";
+        const subjectContent = req.body.subject;
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: emailFrom,
-            pass: pass,
-        },
-        secure: true
-    });
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: emailFrom,
+                pass: pass,
+            },
+            secure: true
+        });
 
-    const mailOptions = {
-        from: emailFrom,
-        to: emailTo,
-    };
+        const mailOptions = {
+            from: emailFrom,
+            to: emailTo,
+        };
 
-    await transporter.sendMail({
-        ...mailOptions,
-        subject: subjectContent,
-        text : content,
-        html : htmlContent     
-    });
+        await transporter.sendMail({
+            ...mailOptions,
+            subject: subjectContent,
+            text: content,
+            html: htmlContent      
+        });
 
-    res.send("Email Send!");
+        res.send("Email Send!");
 
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+})
+
+app.post("/upload", upload.single('file'), async (req, res) => {
+    console.log(req.file);
+    // console.log(req.file.buffer);
+    res.send("done");
+})
+
+app.post("/attachmentMail", upload.single('file'), async (req, res) => {
+    try {
+        // console.log(req.file.buffer);
+        const emailFrom = req.body.from;
+        const pass = req.body.pass;
+        const emailTo = req.body.to;
+        const content = req.body.content;
+        const htmlContent = req.body.html || "";
+        const subjectContent = req.body.subject;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: emailFrom,
+                pass: pass,
+            },
+            secure: true
+        });
+
+        const mailOptions = {
+            from: emailFrom,
+            to: emailTo,
+        };
+
+        await transporter.sendMail({
+            ...mailOptions,
+            subject: subjectContent,
+            text: content,
+            html: htmlContent,
+            attachments: [
+                {
+                    filename: req.file.originalname,
+                    content: req.file.buffer
+                }
+            ]
+        });
+
+        res.send("Email Send!");
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 })
 
 app.listen(port, () => {
